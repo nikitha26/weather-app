9459f60fe1484af0dc9eec40673f19a6c7396a07
nikitha26 <mnikitha365@gmail.com>
2020-12-25 20:49:26 +0530

    improved app

--- src/App.js
+++ src/App.js
@@ -15,15 +15,20 @@ function App()
 {
     const GetCity = async() => {
       var a = document.getElementById("city").value;
-       
+
       if(a == ""){
         alert("Enter City")
       }
-
-      const response = await Axios.get('https://api.openweathermap.org/data/2.5/weather?q='+a+'&appid=69089d672da4c29c1c959cbaf82acdf2')
-      
-      document.getElementById("result").innerHTML =
-      ReactDOMServer.renderToString(<Result response={response}/>);
+      else if(Number.isInteger(+a)){
+          alert("Enter Only City Names")
+        }
+       else if(typeof(a) === 'string')
+      {
+        const response = await Axios.get('https://api.openweathermap.org/data/2.5/weather?q='+a+'&appid=69089d672da4c29c1c959cbaf82acdf2')
+        
+        document.getElementById("result").innerHTML =
+        ReactDOMServer.renderToString(<Result response={response}/>);
+      }
     }
      
   return (
@@ -31,7 +36,7 @@ function App()
       <Row >
           <Col md={4} className="offset-md-4 mt-2 pt-5">
             <h2>WEATHER APPLICATION</h2>
-              <h4 className="mt-5 ml-5">ENTER CITY :</h4>
+              <h4 className="mt-5">ENTER CITY :</h4>
               <Input
                 type="text"
                 id="city"
--- src/Result.js
+++ src/Result.js
@@ -8,23 +8,30 @@ import {Container,
     CardTitle
     } from "reactstrap"
 function Result({response}){
-   //console.log(response)
+   console.log(response)
      var q = response.data
       var x = response.data['name']
+      var country = q['sys']['country']
       var y = q['main']['temp']
       var z = q['weather'][0]['description']
       var deg = q['wind']['deg']
-
+     
+      //Date and time
+      var d = new Date();
+        var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        var months = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"];
+        var timeanddate = d.getHours()+":"+d.getMinutes()+","+months[d.getMonth()]+" "+d.getDate()
     return(
         <Col lg={12} className="mt-4" id="result">
             <Card>
-                <CardBody className="text-center">
-                    <CardTitle>
-                        <h3>CITY :{x}</h3>
+            <p>{timeanddate}</p>
+                    <CardTitle className="text-center">
+                        <h5>CITY :{x},{country}</h5>
+                    </CardTitle> 
+                    <CardBody>     
                         <h6>TEMP :{y}</h6>
                         <h6>DESCRIPTION :{z}</h6>
                         <h6>DEG :{deg}</h6>
-                    </CardTitle>  
                 </CardBody>
             </Card> 
         </Col> 
